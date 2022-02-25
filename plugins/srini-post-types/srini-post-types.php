4d5c2789f2b8a1e83f3f7660fe8d58672dac90e6
Mrityunjay Marol <6997772+mmarol@users.noreply.github.com>
2022-02-25 18:50:59 +0530

    added header and footer options

--- plugins/srini-post-types/srini-post-types.php
+++ plugins/srini-post-types/srini-post-types.php
@@ -13,18 +13,44 @@
  * Domain Path:       /languages
  */
 
-if ( ! defined( 'WPINC')) {
+if (!defined('WPINC')) {
     die;
 }
 
-define( 'SRINI_VERSION', '1.0.0' );
-define( 'SRINIDOMAIN', 'srini-post-types' );
-define( 'SRINIPATH', plugin_dir_path( __FILE__ ) );
+define('SRINI_VERSION', '1.0.0');
+define('SRINIDOMAIN', 'srini-post-types');
+define('SRINIPATH', plugin_dir_path(__FILE__));
 
-require_once( SRINIPATH . '/post-types/register.php');
+require_once(SRINIPATH . '/post-types/register.php');
 add_action('init', 'srini_register_announcement_type');
 add_action('init', 'srini_register_event_type');
 add_action('init', 'srini_register_learning_path_type');
 add_action('init', 'srini_register_book_type');
 add_action('init', 'srini_register_cd_type');
 add_action('init', 'srini_register_article_type');
+add_action('acf/init', 'my_acf_op_init');
+
+function my_acf_op_init()
+{
+    if (function_exists('acf_add_options_page')) {
+
+        acf_add_options_page(array(
+            'page_title'     => 'Global Page Settings',
+            'menu_title'    => 'Global Page Settings',
+            'menu_slug'     => 'global-page-settings',
+            'capability'    => 'edit_posts',
+        ));
+
+        acf_add_options_sub_page(array(
+            'page_title'     => 'Global Header Settings',
+            'menu_title'    => 'Header',
+            'parent_slug'    => 'global-page-settings',
+        ));
+
+        acf_add_options_sub_page(array(
+            'page_title'     => 'Global Footer Settings',
+            'menu_title'    => 'Footer',
+            'parent_slug'    => 'global-page-settings',
+        ));
+    }
+}
